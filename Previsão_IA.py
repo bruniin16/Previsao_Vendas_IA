88dca1c49d1f7c5d8795ad0d2d7cbde668c7f500
Bruno Mesquita <54283663+bruniin16@users.noreply.github.com>
2022-08-31 20:38:00 -0300

    Add files via upload

---
+++ Previsão_IA.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+df = pd.read_csv(r".\advertising.csv")
+
+ia1 = RandomForestRegressor()
+ia2 = LinearRegression()
+
+x = df.drop("Vendas", axis=1)
+y = df["Vendas"]
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=1)
+ia1.fit(x_treino,y_treino)
+ia2.fit(x_treino,y_treino)
+
+result_ia1 = ia1.predict(x_teste)
+result_ia2 = ia2.predict(x_teste)
+
+porcentagem_ia1 = metrics.r2_score(y_teste, result_ia1)
+porcentagem_ia2 = metrics.r2_score(y_teste, result_ia2)
+
+print(f"Certeza da IA1: {porcentagem_ia1:.2%}")
+print(f"Certeza da IA2: {porcentagem_ia2:.2%}")
+
+val_prox_mes = pd.read_csv(r".\novos.csv")
+previsao = ia1.predict(val_prox_mes)
+val_prox_mes["Vendas"] = previsao
+val_prox_mes.to_excel(r".\vendasnovas.xlsx", index=False)
+print(val_prox_mes)
